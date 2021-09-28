60f67e1e3f9327f899737dacb54b6b956804cff5
Vignesh <joshivignesh@users.noreply.github.com>
2021-09-29 00:30:33 +0530

    Updated App file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-     <h2>Hi Tailwind</h2>
+     <h2 className="text-read-500">Hi Tailwind</h2>
       </header>
     </div>
   );
